9fdb224335ff119c46ff9667c988b7fa548f9c69
Saksham Gupta <sakshamdam54321@gmail.com>
2024-05-17 02:37:46 +0530

    handle init command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,31 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // final String command = args[0];
-    //
-    // switch (command) {
-    //   case "init" -> {
-    //     final File root = new File(".git");
-    //     new File(root, "objects").mkdirs();
-    //     new File(root, "refs").mkdirs();
-    //     final File head = new File(root, "HEAD");
-    //
-    //     try {
-    //       head.createNewFile();
-    //       Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
-    //       System.out.println("Initialized git directory");
-    //     } catch (IOException e) {
-    //       throw new RuntimeException(e);
-    //     }
-    //   }
-    //   default -> System.out.println("Unknown command: " + command);
-    // }
-  }
+        // Uncomment this block to pass the first stage
+
+        final String command = args[0];
+
+        switch (command) {
+            case "init" -> {
+                try {
+                    Files.createDirectory(Path.of(".git"));
+                    Files.createDirectory(Path.of(".git", "objects"));
+                    Files.createDirectory(Path.of(".git", "refs"));
+                    Files.writeString(Path.of(".git/HEAD"), "ref: refs/heads/main\n");
+                    System.out.println("Initialized git directory");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            default -> System.out.println("Unknown command: " + command);
+        }
+    }
 }
